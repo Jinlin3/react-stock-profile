503e2ee786e3b13b0240db47a8225d3a4bdd192d
JJ PC <hellojjlin@gmail.com>
2023-08-24 00:18:11 -0400

    finished left side of third header

--- src/components/ThirdHeader.js
+++ src/components/ThirdHeader.js
@@ -1,7 +1,58 @@
+import SearchImg from './images/SearchImg';
+
 const ThirdHeader = () => {
   return (
     <div className="third-header-container">
-      <h1>THIRD HEADER</h1>
+      <div className="inner-container">
+
+        <div className="stock-grid">
+          <div className="recent">Recent</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+          <div className="company-name">SPY</div>
+
+          <div className="markets">Markets</div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+          <div className="stock-status">
+            <div className="stock-price">100.00</div>
+            <div className="percentage">1.00%</div>
+          </div>
+
+        </div>
+        <div className="search-bar-side">
+          <div className="search-button">
+            <SearchImg />
+          </div>
+          <input type="text" placeholder="Search Tickers, Companies, Or News" />
+        </div>
+      </div>
     </div>
   );
 }
