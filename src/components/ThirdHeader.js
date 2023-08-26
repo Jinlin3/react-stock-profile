8624e15ef36f3cac4700921ecd59afd37a3fd08d
JJ PC <hellojjlin@gmail.com>
2023-08-25 20:52:16 -0400

    fixed the key problem in the third header

--- src/components/ThirdHeader.js
+++ src/components/ThirdHeader.js
@@ -2,14 +2,16 @@ import SearchImg from './images/SearchImg';
 import IncreaseArrow from './images/IncreaseArrow';
 import DecreaseArrow from './images/DecreaseArrow';
 
+// will use names as keys
+
 const stockList = [
-  {name: 'SPY', price: 436.95, change: 'down', percentage: '1.37%', id: 1},
-  {name: 'QQQ', price: 361.22, change: 'down', percentage: '2.14%', id: 4},
-  {name: 'SPIKE', price: 17.24, change: 'up', percentage: '6.55%', id: 7},
-  {name: 'BTC/USD', price: 26122.15, change: 'down', percentage: '1.0675%', id: 10},
-  {name: 'DIA', price: 341.03, change: 'down', percentage: '1.1%', id: 13},
-  {name: 'GLD', price: 177.85, change: 'down', percentage: '0.02%', id: 16},
-  {name: 'TLT', price: 94.91, change: 'down', percentage: '0.66%', id: 19}
+  {name: 'SPY', price: 436.95, change: 'down', percentage: '1.37%'},
+  {name: 'QQQ', price: 361.22, change: 'down', percentage: '2.14%'},
+  {name: 'SPIKE', price: 17.24, change: 'up', percentage: '6.55%'},
+  {name: 'BTC/USD', price: 26122.15, change: 'down', percentage: '1.0675%'},
+  {name: 'DIA', price: 341.03, change: 'down', percentage: '1.1%'},
+  {name: 'GLD', price: 177.85, change: 'down', percentage: '0.02%'},
+  {name: 'TLT', price: 94.91, change: 'down', percentage: '0.66%'}
 ];
 
 const ThirdHeader = () => {
@@ -20,25 +22,25 @@ const ThirdHeader = () => {
         <div className="stock-grid">
           <div className="recent">Recent</div>
           {stockList.map((stock) => (
-            <div className="company-name">{ stock.name }</div>
+            <div className="company-name" key={ stock.name }>{ stock.name }</div>
           ))}
 
           <div className="markets">Markets</div>
 
           {stockList.map((stock) => (
-            <div className="stock-status">
-            <div className="stock-price">{ stock.price }</div>
-            <div style={{ color: stock.change === 'up' ? 'rgb(14, 159, 110)' : 'rgb(240, 82, 82)' }} className="stock-change">
-              {stock.change === 'up' ? (
-                <IncreaseArrow />
-              ) : (
-                <DecreaseArrow />
-              )}
-              <div className="stock-percentage">
-              { stock.percentage }
+            <div className="stock-status" key={ stock.name }>
+              <div className="stock-price">{ stock.price }</div>
+              <div style={{ color: stock.change === 'up' ? 'rgb(14, 159, 110)' : 'rgb(240, 82, 82)' }} className="stock-change">
+                {stock.change === 'up' ? (
+                  <IncreaseArrow />
+                ) : (
+                  <DecreaseArrow />
+                )}
+                <div className="stock-percentage">
+                { stock.percentage }
+                </div>
               </div>
             </div>
-          </div>
           ))}
 
         </div>
