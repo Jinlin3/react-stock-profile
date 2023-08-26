effe5382ef2fd8ae0e8bad471234bbb085b56ff8
JJ PC <hellojjlin@gmail.com>
2023-08-25 20:30:47 -0400

    finished CompanyActions component

--- src/components/StockDetail.js
+++ src/components/StockDetail.js
@@ -1,7 +1,24 @@
+import CompanyActions from './CompanyActions';
+
+const companyInfo = {
+  name: 'Apple Inc',
+  abbreviation: 'NASDAQ:APPL',
+
+  atClosePrice: 178.61,
+  atClosePriceChange: 2.23,
+  atClosePercentChange: 1.26,
+  lastCloseTime: 'Aug 25',
+
+  afterHoursPrice: 178.3611,
+  afterHoursPriceChange: -0.2489,
+  afterHoursPercentChange: -0.14,
+  afterHoursTime: '6:24PM EDT'
+}
+
 const StockDetails = () => {
   return (
     <div className="stock-details-container">
-      <h1>STOCK DETAILS</h1>
+      <CompanyActions companyInfo={companyInfo} />
     </div>
   );
 }
--- src/components/ThirdHeader.js
+++ src/components/ThirdHeader.js
@@ -3,13 +3,13 @@ import IncreaseArrow from './images/IncreaseArrow';
 import DecreaseArrow from './images/DecreaseArrow';
 
 const stockList = [
-  {name: 'SPY', price: 436.95, change: 'down', percentage: '1.37%'},
-  {name: 'QQQ', price: 361.22, change: 'down', percentage: '2.14%'},
-  {name: 'SPIKE', price: 17.24, change: 'up', percentage: '6.55%'},
-  {name: 'BTC/USD', price: 26122.15, change: 'down', percentage: '1.0675%'},
-  {name: 'DIA', price: 341.03, change: 'down', percentage: '1.1%'},
-  {name: 'GLD', price: 177.85, change: 'down', percentage: '0.02%'},
-  {name: 'TLT', price: 94.91, change: 'down', percentage: '0.66%'}
+  {name: 'SPY', price: 436.95, change: 'down', percentage: '1.37%', id: 1},
+  {name: 'QQQ', price: 361.22, change: 'down', percentage: '2.14%', id: 4},
+  {name: 'SPIKE', price: 17.24, change: 'up', percentage: '6.55%', id: 7},
+  {name: 'BTC/USD', price: 26122.15, change: 'down', percentage: '1.0675%', id: 10},
+  {name: 'DIA', price: 341.03, change: 'down', percentage: '1.1%', id: 13},
+  {name: 'GLD', price: 177.85, change: 'down', percentage: '0.02%', id: 16},
+  {name: 'TLT', price: 94.91, change: 'down', percentage: '0.66%', id: 19}
 ];
 
 const ThirdHeader = () => {
